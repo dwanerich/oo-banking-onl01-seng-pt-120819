872eb82e4f84bd2114ce7c644207d656d2b24da7
dwane richards <dwanerich@gmail.com>
2020-01-24 17:29:55 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,20 @@
 class BankAccount
 
+  attr_reader :name, :status
+  attr_accessor :balance
+
+@@balance = []
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+
+  end
+
+  def deposit
+    @balance += @balance
+
+  end
+
 end
