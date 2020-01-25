5ff2074868cd85d775a91e1411f3ec0ff2d40d38
dwane richards <dwanerich@gmail.com>
2020-01-25 03:53:14 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,15 +9,26 @@ class Transfer
   end
 
   def valid?
-
+    @sender.valid? && @receiver.valid? ? true : false
   end
 
   def execute_transaction
-
+   if (sender.balance > @amount) && @status == "pending"
+      sender.balance -= @amount
+      receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
   end
 
   def reverse_transfer
-
+    if @status == "complete"
+      receiver.balance -= @amount
+      sender.balance += @amount
+      @status = "reversed"
+    end
   end
 
 
