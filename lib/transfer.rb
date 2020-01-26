88880379b0948afa090c0a0186a0ab61c8e4da98
dwane richards <dwanerich@gmail.com>
2020-01-26 00:40:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,8 @@ class Transfer
 
   def execute_transaction
 
-   if sender.balance > @amount && @status == "pending"
+   if valid?
+     sender.balance > @amount && @status == "pending"
       sender.balance -= @amount
       receiver.balance += @amount
       @status = 'complete'
