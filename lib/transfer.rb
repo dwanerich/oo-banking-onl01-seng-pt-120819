7a1d3837f074b7c83af6c66b09a46e4a53111437
dwane richards <dwanerich@gmail.com>
2020-01-26 00:51:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       sender.balance -= @amount
       receiver.balance += @amount
       @status = 'complete'
-      @sender.valid?
+      @sender.valid? ? true : false
     else
       @status = 'rejected'
       "Transaction rejected. Please check your account balance."
